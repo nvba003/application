bb608f1866f856b06e807802efab4aae5a2ef468
nvba003 <nvba003@gmail.com>
2024-04-07 17:54:32 +0700

    fix send data error

--- app/Http/Controllers/Accounting/AccountingController.php
+++ app/Http/Controllers/Accounting/AccountingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Accounting;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Models\Accounting\AccountingOrder; 
 use App\Models\Accounting\AccountingOrderDetail; 
 
--- routes/api.php
+++ routes/api.php
@@ -28,5 +28,7 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
-    Route::post('/accounting_orders', [AccountingController::class, 'store']);
+    
 });
+
+Route::post('/accounting_orders', [AccountingController::class, 'store']);
