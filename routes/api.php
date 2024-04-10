5abc8b3265e78d1e8fa16a46b8a9cca26e899891
nvba003 <nvba003@gmail.com>
2024-04-10 19:00:57 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -28,9 +28,10 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);  
+    Route::post('/accounting_orders', [AccountingController::class, 'store']);
 });
 
-    Route::post('/accounting_orders', [AccountingController::class, 'store']);
+    
     Route::post('/accounting_recovery', [AccountingController::class, 'recovery']);
     Route::post('/accounting_product_price', [AccountingController::class, 'productPrice']);
 
