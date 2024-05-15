a665dfe1921c9b466e766e5829f792e18871c170
nvba003 <nvba003@gmail.com>
2024-05-16 06:38:06 +0700

    update modal temporary

--- resources/views/accounting/order_temporary.blade.php
+++ resources/views/accounting/order_temporary.blade.php
@@ -107,13 +107,14 @@
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">STT</th>
-              <th scope="col">Mã SAP</th>
-              <th scope="col">Tên sản phẩm</th>
-              <th scope="col">Đơn giá</th>
-              <th scope="col">Số lượng</th>
-              <th scope="col">Chiết khấu</th>
-              <th scope="col">Thành tiền</th>
+              <th scope="col" class="px-2 py-2 text-xs font-bold text-gray-900 uppercase tracking-wider">STT</th>
+              <th scope="col" class="px-2 py-2 text-xs font-bold text-gray-900 uppercase tracking-wider">Mã SAP</th>
+              <th scope="col" class="px-2 py-2 text-xs font-bold text-gray-900 uppercase tracking-wider">Tên sản phẩm</th>
+              <th scope="col" class="px-2 py-2 text-right text-xs font-bold text-gray-900 uppercase tracking-wider">Đơn giá</th>
+              <th scope="col" class="px-2 py-2 text-right text-xs font-bold text-gray-900 uppercase tracking-wider">Số lượng</th>
+              <th scope="col" class="px-2 py-2 text-right text-xs font-bold text-gray-900 uppercase tracking-wider">T.Tiền</th>
+              <th scope="col" class="px-2 py-2 text-right text-xs font-bold text-gray-900 uppercase tracking-wider">C.Khấu</th>
+              <th scope="col" class="px-2 py-2 text-right text-xs font-bold text-gray-900 uppercase tracking-wider">T.Toán</th>
             </tr>
           </thead>
           <tbody id="productSummaryDetails">
@@ -363,13 +364,14 @@ $(document).ready(function() {
         Object.values(products).forEach(product => {
             stt++;
             let row = `<tr>
-                <td>${stt}</td>
-                <td>${product.product_code}</td>
-                <td>${product.product_name}</td>
-                <td class="text-right">${product.price.toLocaleString()}</td>
-                <td class="text-right">${product.quantity}</td>
-                <td class="text-right">${product.totalDiscount.toLocaleString()}</td>
-                <td class="text-right">${product.totalPrice.toLocaleString()}</td>
+                <td class="w-1/12 px-3 py-2 text-sm border-b border-grey-light">${stt}</td>
+                <td class="w-2/12 px-3 py-2 text-sm border-b border-grey-light">${product.product_code}</td>
+                <td class="w-4/12 px-3 py-2 text-sm border-b border-grey-light">${product.product_name}</td>
+                <td class="w-1/12 px-3 py-2 text-sm border-b border-grey-light text-right">${product.price.toLocaleString()}</td>
+                <td class="w-1/12 px-3 py-2 text-sm border-b border-grey-light text-right">${product.quantity}</td>
+                <td class="w-1/12 px-3 py-2 text-sm border-b border-grey-light text-right">${product.subtotal.toLocaleString()}</td>
+                <td class="w-1/12 px-3 py-2 text-sm border-b border-grey-light text-right">${product.totalDiscount.toLocaleString()}</td>
+                <td class="w-1/12 px-3 py-2 text-sm border-b border-grey-light text-right">${product.totalPrice.toLocaleString()}</td>
             </tr>`;
             productDetails.append(row);
         });
@@ -388,10 +390,10 @@ $(document).ready(function() {
         }
         // Thêm hàng tổng kết
         let totalRow = `<tr class="table-info">
-        <td colspan="4"><strong>Tổng cộng</strong></td>
-        <td class="text-right"><strong>${totalQuantity.toLocaleString()}</strong></td>
-        <td class="text-right"><strong>${totalDiscount.toLocaleString()}</strong></td>
-        <td class="text-right"><strong>${totalPayable.toLocaleString()}</strong></td>
+        <td colspan="5"><strong>Tổng cộng</strong></td>
+        <td class="text-sm text-right"><strong>${totalQuantity.toLocaleString()}</strong></td>
+        <td class="text-sm text-right"><strong>${totalDiscount.toLocaleString()}</strong></td>
+        <td class="text-sm text-right"><strong>${totalPayable.toLocaleString()}</strong></td>
         </tr>`;
         productDetails.append(totalRow);
 
